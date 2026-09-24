800a7ac3296ba6f609900a8c7ade574e7d31ff5b
Nurazkhan Nygmet <rominefire@gmail.com>
2026-09-24 10:19:21 +0800

    firmware Done all pages added

--- Firmware/NiroClock.ino
+++ Firmware/NiroClock.ino
@@ -1,45 +1,43 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
 #include <SPI.h>
-#include <string>
-#include <iostream>
+
+// Display SPI Pins
+#define TFT_SCLK 9   // SCL on screen
+#define TFT_MOSI 10  // SDA on screen
+#define TFT_RST  -1
+#define TFT_DC   5
+#define TFT_CS   6
+#define TFT_BL   8
 
 
-//display
-#define TFT_SCLK 9 // labeled SCL on the screen
-#define TFT_MOSI 10// labeled SDA on the screen
-#define TFT_RST -1
-#define TFT_DC 5
-#define TFT_CS 6
-#define TFT_BL 8
+#define BUZZER   7
 
-
-//buzzer
-#define BUZZER 7
-
-//butons
 #define BT5 0
 #define BT4 1
 #define BT3 2
 #define BT2 3
 #define BT1 4
 
+const unsigned long DEBOUNCE_TIME = 150; // ms
+unsigned long clockTick = 0;
+unsigned long buzzerTick = 0;
+bool buzzerState = false;
 
-//time
-unsigned long tick = 0;
-int hours = 12, minutes = 0, seconds =0;
-int alarmHours = 0, alarmMinutes =0;
-bool alarmEnabled = False;
 
-// debounce for btns
-bool LastBt1 = HIGH;
-bool LastBt2 = HIGH;
-bool LastBt3 = HIGH;
-bool LastBt4 = HIGH;
-bool LastBt5 = HIGH;
+int hours = 12, minutes = 0, seconds = 0;
+int alarmHours = 0, alarmMinutes = 0;
+bool alarmEnabled = false;
 
-//debounce time
-const unsigned long = 150; //ms
+int secondmeter[3] = {0, 0, 0}; // H, M, S
+bool secondmeterEnabled = false;
+
+
+bool lastBt1 = HIGH;
+bool lastBt2 = HIGH;
+bool lastBt3 = HIGH;
+bool lastBt4 = HIGH;
+bool lastBt5 = HIGH;
 
 
 class MyST7789 : public Adafruit_ST7789 {
@@ -54,180 +52,333 @@ public:
 
 MyST7789 tft(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
-enum Mode {State_Clock,State_Alarm, State_Settings, State_SetClock,State_SetAlarm, State_Secondmeter};
-enum ClockMode {HMS, HM};
+enum Mode { State_Clock, State_Alarm, State_SetClock, State_SetAlarm, State_Secondmeter };
+enum ClockMode { HMS, HM };
 
 Mode currentState = State_Clock;
-ClockMode CurrentClockMode = HMS;
+ClockMode currentClockMode = HMS;
 
-void drawClock(){
-  if(currentState != State_Clock){
-    return; 
-  }
-  tft.setCursor(20,20);
-  tft.setTextSize(6);
-  if (CurrentClockMode == HMS){
-    char timeBuffer[9]; //HH:MM:SS 9 symbols
-    snprintf(timeBuffer, sizeof(timeBuffer), "%02d:%02d:%02d", hours, minutes, seconds);
-  }else{
-    char timeBuffer[5]; // HH:MM 5 symbol
-    snprintf(timeBuffer,sizeof(timeBuffer), "%02d:%02d", hours, minutes)
-  }
-  
-  tft.print(timebuffer);
-}
-void drawSetClock(int[] snapshotTime){
-  if(currentState != State_SetClock){
-    return; 
-  }
-  tft.setCursor(20,20);
-  tft.setTextSize(6);
- 
-    char timeBuffer[9]; //HH:MM:SS 9 symbols
-    snprintf(timeBuffer, sizeof(timeBuffer), "%02d:%02d:%02d", snapshotTime[0], snapshotTime[1], snapshotTime[2]);
-  
-  
-  tft.print(timebuffer);
-};
+// Setting buffers
+int editTime[3] = {0, 0, 0}; // [H, M, S]
+int editPointer = 0;
 
-bool checkPressed(int pin, bool lastState){
+
+bool checkPressed(int pin, bool &lastState) {
   int currentPressed = digitalRead(pin);
-  if (currentPressed == LOW && lastState == HIGH ){
+  if (currentPressed == LOW && lastState == HIGH) {
     lastState = LOW;
-    delay(20);
+    delay(20); 
     return true;
   }
-  lastState = currentPressed;
+  if (currentPressed == HIGH && lastState == LOW) {
+    lastState = HIGH;
+  }
   return false;
-};
+}
 
 
-void ClockPageButtons(){
-  if(currentState != State_Clock){
+void drawClock() {
+  tft.setCursor(20, 20);
+  tft.setTextSize(4);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+
+  char timeBuffer[12];
+  if (currentClockMode == HMS) {
+    snprintf(timeBuffer, sizeof(timeBuffer), "%02d:%02d:%02d", hours, minutes, seconds);
+  } else {
+    snprintf(timeBuffer, sizeof(timeBuffer), "%02d:%02d   ", hours, minutes);
+  }
+  tft.print(timeBuffer);
+
+  // Status line
+  tft.setCursor(20, 60);
+  tft.setTextSize(2);
+  tft.printf("Alarm: %s [%02d:%02d]", alarmEnabled ? "ON " : "OFF", alarmHours, alarmMinutes);
+}
+
+void drawSetClock() {
+  tft.setCursor(20, 15);
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
+  tft.print("Set Time (H:M:S)   ");
+
+  tft.setCursor(20, 40);
+  tft.setTextSize(4);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  char buffer[12];
+  snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d", editTime[0], editTime[1], editTime[2]);
+  tft.print(buffer);
+
+  
+  tft.setCursor(20, 75);
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_CYAN, ST77XX_BLACK);
+  if (editPointer == 0)      tft.print("Selected: HOURS  ");
+  else if (editPointer == 1) tft.print("Selected: MINUTES");
+  else                       tft.print("Selected: SECONDS");
+}
+
+void drawSetAlarm() {
+  tft.setCursor(20, 15);
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
+  tft.print("Set Alarm (H:M)    ");
+
+  tft.setCursor(20, 40);
+  tft.setTextSize(4);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  char buffer[10];
+  snprintf(buffer, sizeof(buffer), "%02d:%02d   ", editTime[0], editTime[1]);
+  tft.print(buffer);
+
+  tft.setCursor(20, 75);
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_CYAN, ST77XX_BLACK);
+  if (editPointer == 0) tft.print("Selected: HOURS  ");
+  else                  tft.print("Selected: MINUTES");
+}
+
+void drawSecondmeter() {
+  tft.setCursor(20, 15);
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_YELLOW, ST77XX_BLACK);
+  tft.print("Stopwatch          ");
+
+  tft.setCursor(20, 40);
+  tft.setTextSize(4);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  char buffer[12];
+  snprintf(buffer, sizeof(buffer), "%02d:%02d:%02d", secondmeter[0], secondmeter[1], secondmeter[2]);
+  tft.print(buffer);
+}
+
+void drawAlarm() {
+  tft.setCursor(20, 15);
+  tft.setTextSize(3);
+  tft.setTextColor(ST77XX_RED, ST77XX_BLACK);
+  tft.print("WAKE UP!   ");
+
+  tft.setCursor(20, 50);
+  tft.setTextSize(4);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+  char timeBuffer[10];
+  snprintf(timeBuffer, sizeof(timeBuffer), "%02d:%02d", alarmHours, alarmMinutes);
+  tft.print(timeBuffer);
+}
+
+
+void handleBuzzer() {
+  if (currentState != State_Alarm) {
+    digitalWrite(BUZZER, LOW);
+    buzzerState = false;
     return;
-  };
-  if(checkPressed(BT1, lastBt1)){
-    CurrentClockMode = CurrentClockMode == HMS ? HM:HMS;
-  };
-  if(checkPressed(BT2, lastBt2)){
+  }
+
+  // every 300s beep 
+  if (millis() - buzzerTick >= 300) {
+    buzzerTick = millis();
+    buzzerState = !buzzerState;
+    digitalWrite(BUZZER, buzzerState ? HIGH : LOW);
+  }
+}
+
+
+
+void ClockPageButtons() {
+  drawClock();
+
+  if (checkPressed(BT1, lastBt1)) {
+    currentClockMode = (currentClockMode == HMS) ? HM : HMS;
+    tft.fillScreen(ST77XX_BLACK);
+  }
+  if (checkPressed(BT2, lastBt2)) {
+    tft.fillScreen(ST77XX_BLACK);
     currentState = State_Secondmeter;
-  };
-  if(checkPressed(BT4, lastBt4)){
+  }
+  if (checkPressed(BT4, lastBt4)) {
+    editTime[0] = hours;
+    editTime[1] = minutes;
+    editTime[2] = seconds;
+    editPointer = 0;
+    tft.fillScreen(ST77XX_BLACK);
     currentState = State_SetClock;
-  };
-  if(checkPressed(BT5, lastBt5)){
+  }
+  if (checkPressed(BT5, lastBt5)) {
+    editTime[0] = alarmHours;
+    editTime[1] = alarmMinutes;
+    editPointer = 0;
+    tft.fillScreen(ST77XX_BLACK);
     currentState = State_SetAlarm;
   }
-};
+}
 
-void SetClockPageButtons(){
-  if(currentState != State_SetClock){
-    return;
-  };
-  int pointer = 0; //max value 2
-  /*  H:M:S
-      ^   
-      |  */
-  int[] snapshotTime = [hours,minutes,seconds]; 
-  drawSetClock(snapshotTime);
-    // move left
-   if(checkPressed(BT1, lastBt1)){
-    pointer -=1;
-    pointer= pointer < 0 ? 2:pointer;
-  };
-  //move right
-  if(checkPressed(BT2, lastBt2)){
-    pointer +=1;
-    pointer= pointer > 2 ? 0:pointer;
-  };
-  //move up
-  if(checkPressed(BT3, lastBt3)){
-    snapshotTime[pointer] += 1;
-    if(pointer == 0 && snapshotTime[pointer] > 23){
-      snapshotTime[pointer] = 0
-    }else if ((pointer == 1 || pointer == 2) && snapshotTime[pointer] > 59)
-    {
-      snapshotTime[pointer] = 0
-    }};
-    // move down
-    if(checkPressed(BT4, lastBt4)){
-    snapshotTime[pointer] -= 1;
-    if(pointer == 0 && snapshotTime[pointer] < 0){
-      snapshotTime[pointer] = 23
-    }else if ((pointer == 1 || pointer == 2) && snapshotTime[pointer] < 0)
-    {
-      snapshotTime[pointer] = 59
-    };};
-    // Save and leave
-    if(checkPressed(BT5, lastBt5)){
-      hours = snapshotTime[0];
-      minutes = snapshotTime[1];
-      seconds = snapshotTime[2];
-      currentState = State_SetClock;
-    };
-    
-  };
+void SetClockPageButtons() {
+  drawSetClock();
+ // Left
+  if (checkPressed(BT1, lastBt1)) {
+    editPointer = (editPointer <= 0) ? 2 : editPointer - 1;
+  }
+  // Right
+  if (checkPressed(BT2, lastBt2)) { 
+    editPointer = (editPointer >= 2) ? 0 : editPointer + 1;
+  }
+  if (checkPressed(BT3, lastBt3)) { // Up
+    int maxVal = (editPointer == 0) ? 23 : 59;
+    editTime[editPointer] = (editTime[editPointer] + 1) > maxVal ? 0 : editTime[editPointer] + 1;
+  }
+  if (checkPressed(BT4, lastBt4)) { // Down
+    int maxVal = (editPointer == 0) ? 23 : 59;
+    editTime[editPointer] = (editTime[editPointer] - 1) < 0 ? maxVal : editTime[editPointer] - 1;
+  }
+  if (checkPressed(BT5, lastBt5)) { // Save & Exit
+    hours = editTime[0];
+    minutes = editTime[1];
+    seconds = editTime[2];
+    tft.fillScreen(ST77XX_BLACK);
+    currentState = State_Clock;
+  }
+}
 
-void SetAlarmPageButtons(){
+void SetAlarmPageButtons() {
+  drawSetAlarm();
 
+  if (checkPressed(BT1, lastBt1)) { // Left
+    editPointer = (editPointer <= 0) ? 1 : editPointer - 1;
+  }
+  if (checkPressed(BT2, lastBt2)) { // Right
+    editPointer = (editPointer >= 1) ? 0 : editPointer + 1;
+  }
+  if (checkPressed(BT3, lastBt3)) { // Up
+    int maxVal = (editPointer == 0) ? 23 : 59;
+    editTime[editPointer] = (editTime[editPointer] + 1) > maxVal ? 0 : editTime[editPointer] + 1;
+  }
+  if (checkPressed(BT4, lastBt4)) { // Down
+    int maxVal = (editPointer == 0) ? 23 : 59;
+    editTime[editPointer] = (editTime[editPointer] - 1) < 0 ? maxVal : editTime[editPointer] - 1;
+  }
+  if (checkPressed(BT5, lastBt5)) { // Save & Exit
+    alarmHours = editTime[0];
+    alarmMinutes = editTime[1];
+    alarmEnabled = true;
+    tft.fillScreen(ST77XX_BLACK);
+    currentState = State_Clock;
+  }
+}
+
+void SecondmeterPageButtons() {
+  drawSecondmeter();
+
+  if (checkPressed(BT1, lastBt1)) {
+    secondmeterEnabled = !secondmeterEnabled;
+  }
+  if (checkPressed(BT3, lastBt3)) { // Reset
+    secondmeterEnabled = false;
+    secondmeter[0] = 0;
+    secondmeter[1] = 0;
+    secondmeter[2] = 0;
+  }
+  if (checkPressed(BT4, lastBt4) || checkPressed(BT5, lastBt5)) { // Back
+    tft.fillScreen(ST77XX_BLACK);
+    currentState = State_Clock;
+  }
+}
+
+void AlarmPageButtons() {
+  drawAlarm();
+
+  
+  if (checkPressed(BT1, lastBt1) || checkPressed(BT3, lastBt3) ||
+      checkPressed(BT4, lastBt4) || checkPressed(BT5, lastBt5)) {
+    alarmEnabled = false;
+    digitalWrite(BUZZER, LOW);
+    tft.fillScreen(ST77XX_BLACK);
+    currentState = State_Clock;
+  }
+  // Snooze for 5 minutes
+  if (checkPressed(BT2, lastBt2)) {
+    alarmMinutes += 5;
+    alarmHours += alarmMinutes / 60;
+    alarmMinutes %= 60;
+    alarmHours %= 24;
+    digitalWrite(BUZZER, LOW);
+    tft.fillScreen(ST77XX_BLACK);
+    currentState = State_Clock;
+  }
 }
 
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(TFT_BL, OUTPUT); 
-  digitalWrite(TFT_BL, LOW);
-
-  //btns
-pinMode(BT1, INPUT_PULLUP);
-pinMode(BT2, INPUT_PULLUP);
-pinMode(BT3, INPUT_PULLUP);
-pinMode(BT4, INPUT_PULLUP);
-pinMode(BT5, INPUT_PULLUP);
-
-
-//display setup
-tft.init(76, 284); // Our panel size (portrait)
-tft.setOffsets(82, 18); // Offsets for the weird resolution
-tft.invertDisplay(false); // Invert the colors (This display is flipped from normal)
-tft.setRotation(1); // Landscape, if it's upside down use 3!
-tft.fillScreen(ST77XX_BLACK); // clear the screen
-Serial.println("TFT Initialized!");
-tft.setCursor(0,0); 
-
-//buzzer
-pinMode(BUZZER, OUTPUT);
-
 
   
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH); // Turn backlight ON
+// Buzzer
+  pinMode(BUZZER, OUTPUT);
+  digitalWrite(BUZZER, LOW);
 
+  // Buttons
+  pinMode(BT1, INPUT_PULLUP);
+  pinMode(BT2, INPUT_PULLUP);
+  pinMode(BT3, INPUT_PULLUP);
+  pinMode(BT4, INPUT_PULLUP);
+  pinMode(BT5, INPUT_PULLUP);
+
+  // Display init
+  tft.init(76, 284);
+  tft.setOffsets(82, 18);
+  tft.invertDisplay(false);
+  tft.setRotation(1);
+  tft.fillScreen(ST77XX_BLACK);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(millis() - tick >= 1){
-    tick = millis();
-    seconds +=1;
-    if (seconds >59){
+  
+  if (millis() - clockTick >= 1000) {
+    clockTick = millis();
+
+    seconds++;
+    if (seconds >= 60) {
       seconds = 0;
-      minutes +=1;
-      if(minutes > 59){
+      minutes++;
+      if (minutes >= 60) {
         minutes = 0;
-        hours +=1;
-        if(hours > 23){
-          hours = 0
+        hours = (hours + 1) % 24;
+      }
+    }
+
+    if (secondmeterEnabled) {
+      secondmeter[2]++;
+      if (secondmeter[2] >= 60) {
+        secondmeter[2] = 0;
+        secondmeter[1]++;
+        if (secondmeter[1] >= 60) {
+          secondmeter[1] = 0;
+          secondmeter[0] = (secondmeter[0] + 1) % 24;
         }
       }
     }
-    
-    drawClock();
-  };
-  
-  
-}
 
+    
+    if (alarmEnabled && hours == alarmHours && minutes == alarmMinutes && seconds == 0) {
+      currentState = State_Alarm;
+      tft.fillScreen(ST77XX_BLACK);
+    }
+  }
+
+ // pages and handling them properly with control functions
+  switch (currentState) {
+    case State_Clock:       ClockPageButtons();       break;
+    case State_SetClock:    SetClockPageButtons();    break;
+    case State_SetAlarm:    SetAlarmPageButtons();    break;
+    case State_Secondmeter: SecondmeterPageButtons(); break;
+    case State_Alarm:       AlarmPageButtons();       break;
+  }
+
+  
+  handleBuzzer();
+}
 
 
 // 5 buttons role in each state
